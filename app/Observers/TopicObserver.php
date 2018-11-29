56d66bd7a6a5a0e9d57e64b987308c21503152a5
vagrant <vagrant@homestead>
2018-11-29 07:19:18 +0000

    add queue for translate slug

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -5,6 +5,8 @@ namespace App\Observers;
 use App\Models\Topic;
 use App\Handlers\SlugTranslateHandler;
 
+//use App\Jobs\TranslateSlug;
+
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
 
@@ -16,6 +18,8 @@ class TopicObserver
         $topic->excerpt = make_excerpt($topic->body);
         // 如 slug 字段无内容，即使用翻译器对 title 进行翻译
         if ( ! $topic->slug) {
+            // 推送任务到队列
+            //dispatch(new TranslateSlug($topic));
             $topic->slug = app(SlugTranslateHandler::class)->pinyin($topic->title);
         }
     }
--- config/database.php
+++ config/database.php
@@ -110,8 +110,10 @@ return [
 
         'default' => [
             'host' => env('REDIS_HOST', '127.0.0.1'),
-            'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', 6379),
+            'parameters'=>[
+                'password'=>env('REDIS_PASSWORD', 123456)
+            ],
             'database' => 0,
         ],
 
